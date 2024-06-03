4b64ddfe5e51be0a52c667ae4325dd1c02838ba2
ABHISHEK@ABHI <abhishekgautam.916@gmail.com>
2024-06-03 20:46:03 +0530

    subnet code Addedd with module also vnet code modification

--- AAP_Project/Enviroment/Dev/provider.tf
+++ AAP_Project/Enviroment/Dev/provider.tf
@@ -5,6 +5,12 @@ terraform {
       version = "3.106.1"
     }
   }
+  backend "azurerm" {
+    resource_group_name  = "AbhishekKeyvalutRG_donotdelete"  # Can be passed via `-backend-config=`"resource_group_name=<resource group name>"` in the `init` command.
+    storage_account_name = "storageaccountstatefile"                      # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
+    container_name       = "tfstatefilecontainer"                       # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
+    key                  = "dev.terraform.tfstate" 
+}
 }
 
 provider "azurerm" {
